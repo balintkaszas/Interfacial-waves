e6863cb192fca8f7b6edfd78c905875d80b3e753
Bálint Kaszás <balint.kaszas.7cb@gmail.com>
2018-10-18 01:10:25 +0200

    File output works correctly

--- ex7_original/main.f95
+++ ex7_original/main.f95
@@ -43,12 +43,12 @@ CALL dyn
 ! data output
 IF(MOD(n,nout)==0)THEN
   DO i = 1,nz
-    WRITE(10,'(101F12.6)')(q(i,k)/(RHOREF*G),k=1,nx)
-    WRITE(20,'(101F12.6)')(u(i,k),k=1,nx)
-    WRITE(30,'(101F12.6)')(w(i,k),k=1,nx)
-    WRITE(50,'(101F12.6)')(rho(i,k)-RHOREF,k=1,nx)
+    WRITE(10,'(501F12.6)')(q(i,k)/(RHOREF*G),k=1,nx)
+    WRITE(20,'(501F12.6)')(u(i,k),k=1,nx)
+    WRITE(30,'(501F12.6)')(w(i,k),k=1,nx)
+    WRITE(50,'(501F12.6)')(rho(i,k)-RHOREF,k=1,nx)
   END DO
-  WRITE(40,'(101F12.6)')(q(0,k)/(RHOREF*G),k=1,nx)
+  WRITE(40,'(501F12.6)')(q(0,k)/(RHOREF*G),k=1,nx)
   WRITE(6,*)"Data output at time = ",time/(24.*3600.)
 ENDIF
 
